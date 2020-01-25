de588e7c407f59715a0d6a4c9e1adfca07c0615a
Shigeyuki-fukuda <urbansonnet1990@gmail.com>
2020-01-25 14:47:12 +0900

    :mag: キーワード検索を画面上から行えるようにした

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -1,5 +1,11 @@
 class MangasController < ApplicationController
   def index
-    @mangas = Manga.all
+    @mangas = query.present? ? Manga.search(query).records : Manga.all
   end
+
+  private
+
+    def query
+      @query ||= params[:query]
+    end
 end
